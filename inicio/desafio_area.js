ff57568880e6d878688a04e66af135529ca769ae
Jonathan Brunholi Do Nascimento <Jonathanbnasc@gmail.com>
2022-02-09 16:28:21 -0300

    update

---
+++ inicio/desafio_area.js
@@ -0,0 +1,8 @@
+const raio = 10;
+const PI = 3.141592;
+let areaCirc = PI * raio * raio;
+
+console.log("A área da circunferência é: " + areaCirc + "m²");
+
+areaCirc = Math.PI * raio * raio;
+console.log("A área da circunferência é: " + areaCirc + "m²");
