ca3e5fd12782e9f4e3a584f61b54af87900e6db2
Jonathan Brunholi Do Nascimento <Jonathanbnasc@gmail.com>
2022-02-09 15:12:02 -0300

    atualização

---
+++ inicio/blocos.js
@@ -0,0 +1,18 @@
+{
+    console.log("primeiro passo");
+    console.log("segundo passo");
+    console.log("terceiro passo");
+}
+
+{
+    console.log("quarto passo");
+    console.log("quinto passo");
+}
+
+{
+    {
+
+    }
+}
+
+console.log("Fim!");
